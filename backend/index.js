106772f73ff5e9eeeb87ca834fcd199786a76113
ryowuandjanet <ryowu0329@gmail.com>
2024-05-26 15:37:26 +0800

    NewCollections

--- backend/index.js
+++ backend/index.js
@@ -182,6 +182,12 @@ app.post("/login", async (req, res) => {
   }
 });
 
+app.get("/newcollections", async (req, res) => {
+  let products = await Product.find({});
+  let newcollection = products.slice(1).slice(-8);
+  console.log("NewCollection Fetched");
+  res.send(newcollection);
+});
 app.listen(port, (error) => {
   if (!error) {
     console.log("Server Running on Port 4000");
